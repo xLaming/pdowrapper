d0c587f0e9831fba3389fed25b4cfca684b6470f
Paulo Rodriguez <xlaming@programmer.net>
2018-03-17 18:04:50 -0300

    Create PDO.php

---
+++ PDO.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * *** PDO WRAPPER ***
+ * This class is used to connect to PDO(Wrapper method). It will make the connection easier.
+ * @copyright MIT License. Copyright (c) 2018 Paulo Rodriguez
+ * @author Paulo Rodriguez(xLaming)
+ * @link https://github.com/xlaming/pdowrapper
+ */
+class PDOWrapper
+{
+	/**
+	 * Set-up the driver used to connect to database and also the charset.
+	 * @var array
+	 */
+	private $settings = [
+		'driver'  => 'mysql', // driver method
+		'charset' => 'utf8' // charset type
+	];
+
+	/**
+	 * Set-up some attributes that will be used in the connection method.
+	 * @var array
+	 */
+	private $options  = [
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => false,
+	];
+
+	/**
+	 * Store the connection to use in other functions.
+	 * @var string
+	 */
+	private $link;
+
+	/**
+	 * Store the result to use in other functions.
+	 * @var string
+	 */
+	private $result;
+
+	/**
+	 * This will connect to the database.
+	 * @param string $host
+	 * @param string $user
+	 * @param string $pass
+	 * @param string $dbname
+	 * @return string
+	 */
+	public function __construct($host, $user, $pass, $dbname)
+	{
+		try
+		{
+			$setDriver  = "{$this->settings['driver']}:host={$host};dbname={$dbname};charset={$this->settings['charset']}";
+			$this->link = new PDO($setDriver, $user, $pass, $this->options);
+		}
+		catch (PDOException $e)
+		{
+			 echo $e->getMessage();
+			 exit;
+		}
+	}
+
+	/**
+	 * Execute query directly on the driver.
+	 * Example: $sql->query('DELETE FROM users WHERE id = 1')
+	 * @param  string $string
+	 * @return mixed
+	 */
+	public function query($string)
+	{
+		if (!is_string($string))
+		{
+			return false;
+		}
+		$this->result = $this->link->query($string);
+		return $this->result;
+	}
+
+	/**
+	 * Fetch data in the database using the connection driver.
+	 * Example: $sql->fetch_array("SELECT id FROM users WHERE user = 'admin'");
+	 * @param  string $string
+	 * @return mixed
+	 */
+	public function fetch_array($string)
+	{
+		if (!is_string($string))
+		{
+			return false;
+		}
+		$this->query($string);
+		$result = $this->result->fetchAll();
+		return $result;
+	}
+
+	/**
+	 * Insert data to the database using the connection driver.
+	 * Example: $sql->insert('test', ['id' => 10, 'name' => 'Paulo', 'age' => '18'])
+	 * @param  string $table
+	 * @param  array $values
+	 * @return mixed
+	 */
+	public function insert($table, $values)
+	{
+		if (!is_string($table) || !is_array($values))
+		{
+			return false;
+		}
+		$keys    = implode(", ", array_keys($values));
+		$values  = array_values($values);
+		$prepare = $this->link->prepare("INSERT INTO {$table} ({$keys}) VALUES (?, ?, ?)");
+		$prepare->execute($values);
+		return $prepare;
+	}
+
+	/**
+	 * Search data in the database using the connection driver.
+	 * Example: $sql->search('test', ['name', 'Paulo'])
+	 * @param  string $table
+	 * @param  array $values
+	 * @return mixed
+	 */
+	public function search($table, $values)
+	{
+		if (!is_string($table) || !is_array($values) || count($values) < 2)
+		{
+			return false;
+		}
+		$result = $this->fetch_array("SELECT * FROM {$table} WHERE {$values[0]} LIKE '%{$values[1]}%'");
+		if (empty($result))
+		{
+			return false;
+		}
+		return $result;
+	}
+
+	/**
+	 * Count all rows existant in one table.
+	 * Example: $sql->rowCount("users")
+	 * @param  string
+	 * @return int
+	 */
+	public function rowCount($table)
+	{
+		if (!is_string($table))
+		{
+			return false;
+		}
+		$result = $this->fetch_array("SELECT * FROM {$table}");
+		return count($result);
+	}
+
+	/**
+	 * Count all columns existant in one database. (may be useful for debugs)azZ
+	 * @return int
+	 */
+	public function columnCount()
+	{
+		$result = $this->fetch_array("SHOW TABLES");
+		return count($result);
+	}
+
+	/**
+	 * Search the last inserted value.
+	 * @return int
+	 */
+	public function lastInsertId()
+	{
+		return $this->link->lastInsertId();
+	}
+}
+?>
